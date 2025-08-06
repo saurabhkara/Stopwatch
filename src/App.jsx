e0cb20f365b0500dc30b697ee9f7d16a99ba8f87
Saurabh <saurabkumar679@gmail..com>
2025-08-06 13:30:01 +0530

    project done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { useRef, useState } from "react";
-
 import "./App.css";
+import { useRef, useState } from "react";
 
 function App() {
   const [time, setTime] = useState(0);
@@ -8,26 +7,48 @@ function App() {
   const timerIdRef = useRef(null);
 
   const handleStart = () => {
+    if (timerIdRef.current) {
+      return;
+    }
     startTimeRef.current = new Date().getTime() - time;
     timerIdRef.current = setInterval(() => {
       let diffTime = new Date().getTime() - startTimeRef.current;
       setTime(diffTime);
     }, 10);
-    console.log(startTimeRef.current);
   };
 
-  function handlePause() {
+  const handlePause = () => {
+    console.log("paused");
     clearInterval(timerIdRef.current);
-  }
+    timerIdRef.current = null;
+  };
 
   function reset() {
     clearInterval(timerIdRef.current);
     setTime(0);
   }
+
+  function formatTime() {
+    const milSec = Math.floor((time % 1000) / 10)
+      .toString()
+      .padStart(2, "0");
+    const second = Math.floor((time / 1000) % 60)
+      .toString()
+      .padStart(2, "0");
+    const minute = Math.floor((time / (1000 * 60)) % 60)
+      .toString()
+      .padStart(2, "0");
+    const hour = Math.floor(time / (1000 * 60 * 60))
+      .toString()
+      .padStart(2, "0");
+    return `${hour}:${minute}:${second}:${milSec}`;
+  }
   return (
     <div>
       <h4>Count Down timer</h4>
-      <h1>{time}</h1>
+      <div className="time">
+        <h1 className="formatedTimeText">{formatTime()}</h1>
+      </div>
       <div>
         <button onClick={handleStart}>Start</button>
         <button onClick={handlePause}>Pause</button>
